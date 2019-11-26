23a1ad57d1b370ee034e8fdcf12114d73a83cdad
Anand Pandey <anandp332@gmail.com>
2019-11-26 12:42:35 +0530

    25 Nov

---
+++ MyClassRoom/Nov25/ondrop.js
@@ -0,0 +1,10 @@
+ondrop
+ondragover
+ondragstart
+etc.
+
+Note:
+
+	event.preventDefault() is a predefined method using which we could able to prevent the default actions happens on an event generation.
+
+	
---
+++ MyClassRoom/Nov25/worker.onmessage() - Predefined method gets invoke.js
@@ -0,0 +1,5 @@
+worker.onmessage() - Predefined method gets invoked automatically once there is  a message from a worker
+
+worker.terminate() - through we can stop receiving messages from a worker
+
+postMessage() - This messagge is used inside a worker js file, using which we could post a message to mainthread. 
